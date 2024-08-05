12552f349930b92b03ac1a18ba9c5631933b9155
gaamita <71674219+gaamita@users.noreply.github.com>
2024-08-05 12:30:15 -0700

    Update step_trainer_trusted.py

--- scripts/step_trainer_trusted.py
+++ scripts/step_trainer_trusted.py
@@ -35,6 +35,6 @@ on otherSource.serialNumber = myDataSource.serialNumber
 SQLQuery_node1722307480866 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"myDataSource":customer_curated_node1722307442852, "otherSource":step_trainer_landing_node1722307428733}, transformation_ctx = "SQLQuery_node1722307480866")
 
 # Script generated for node step_trainer_trusted
-step_trainer_trusted_node1722308453235 = glueContext.write_dynamic_frame.from_catalog(frame=SQLQuery_node1722307480866, database="stedi", table_name="step_trainer_trusted", transformation_ctx="step_trainer_trusted_node1722308453235")
+step_trainer_trusted_node1722308453235 = glueContext.write_dynamic_frame.from_catalog(frame=SQLQuery_node1722307480866, database="stedi", table_name="step_trainer_trusted", additional_options={"enableUpdateCatalog": True, "updateBehavior": "UPDATE_IN_DATABASE"}, transformation_ctx="step_trainer_trusted_node1722308453235")
 
 job.commit()
